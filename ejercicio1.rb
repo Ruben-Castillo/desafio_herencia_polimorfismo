6e280e7ea1d568c3059a7fe6f2ff1ae44f1c8bd7
Rubén Ignacio Castillo Díaz <ruben.cas@outlook.com>
2020-10-25 17:56:10 -0300

    initial commit

---
+++ ejercicio1.rb
@@ -0,0 +1,45 @@
+class Person 
+    attr_reader :first_name, :last_name, :age, :type
+    def initialize (first,last,age)
+        @first_name = first
+        @last_name = last
+        @age = age
+    end
+    def birthday
+        @age += 1
+    end
+    def talk
+        "Qué querí' que te diga?"
+    end
+    def introduce
+        "Hola, soy una persona, pero no se qué hacer con mi vida"
+    end
+end
+
+class Student < Person
+    def talk
+        "Aquí es la clase de programación con Ruby?"
+    end
+    def introduce
+        "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+    end
+
+end
+
+class Teacher < Person
+    def talk
+        "Bienvenidos a la clase de programación con Ruby!"
+    end
+    def introduce
+        "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+    end
+end
+
+class Parent < Person
+    def talk
+        "Aquí es la reunión de apoderados?"
+    end
+    def introduce
+        "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #{@last_name}"
+    end
+end
